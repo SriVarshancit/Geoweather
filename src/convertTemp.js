40878257bce74de292044ba0a452e08e03203cf0
SriVarshancit <87161674+SriVarshancit@users.noreply.github.com>
2021-11-30 16:36:45 +0530

    Delete src directory

--- src/convertTemp.js
+++
@@ -1,3 +0,0 @@
-const convertTemp = fDegrees => (((fDegrees - 273.15) * 9) / 5 + 32).toFixed(0);
-
-export default convertTemp;
--- src/utils/convertUppercase.js
+++
@@ -1,8 +0,0 @@
-// CONVERT FIRST LETTERS TO UPPERCASE
-const convertUppercase = string =>
-  string
-    .split(' ')
-    .map(letter => letter.charAt(0).toUpperCase() + letter.substring(1))
-    .join(' ');
-
-export default convertUppercase;
